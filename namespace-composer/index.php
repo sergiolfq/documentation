c1f4155597d7875033fd34dfd1f889239ac2f0af
sergiolf <63725987+sergiolf@users.noreply.github.com>
2021-01-17 00:18:49 +0100

    installation of ramdon library class and usage in composer

---
+++ namespace-composer/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+require __DIR__.'\vendor\autoload.php';
+
+use Rych\Random\Random; 
+
+$random = new Random();
+
+$randomNumber = $random->getRandomInteger(1, 100);
+
+echo $randomNumber;
+
